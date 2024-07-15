fe0798f0c6ee9e7b8e271be776b9e99c246d071e
fahmiirfanfaiz <fahmiirfanfaiz01@gmail.com>
2024-07-15 20:34:57 +0700

    menambahkan animasi agenda

--- src/app/agenda/rundown/day1.tsx
+++ src/app/agenda/rundown/day1.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { useState } from "react";
+import {useEffect, useState } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const RUNDOWN = [
   {
@@ -37,41 +39,52 @@ const RUNDOWN = [
 
 const Timeline = ({item, index} : {item:any, index:number}) => {
   const [isOpen, setIsOpen] = useState(false)
+  
   function handleOpen(){
     setIsOpen(!isOpen)
   }
   return(
     <div 
+
     key={index}
     className="flex justify-center gap-x-[1vw]"
     onClick={handleOpen}
     >
       <div 
-      className={"font-publica-sans font-medium text-[1.7vw] pt-[1.25vw] text-right w-[5vw]" + (isOpen?" text-[#EA5571]":" text-[#CD4258]")}
+      className={
+         `font-publica-sans font-medium text-[1.7vw] pt-[1.25vw] text-right w-[5vw] transition-colors duration-300 ease-linear ${isOpen ? "text-[#EA5571]" : "text-[#CD4258]"}`
+        // "font-publica-sans font-medium text-[1.7vw] pt-[1.25vw] text-right w-[5vw]" + (isOpen?" text-[#EA5571]":" text-[#CD4258]")
+      }
       >
         {item.time}
       </div>
       <div
-      className="flex flex-col w-[3vw] items-center gap-y-[0.3vw] pt-[1.25vw]"
+      className="flex flex-col w-[3vw] items-center gap-y-[0.2vw] pt-[1.25vw]"
       >
         <div 
-        className={"w-[1.7vw] h-[2vw] rounded-full bg-[#CD4258] drop-shadow-[0_0.3vw_0.2vw_#000000]" + (isOpen?" bg-[#EA5571]":" bg-[#CD4258]")}
+        className={
+          `w-[1.7vw] h-[2vw] rounded-full drop-shadow-[0_0.3vw_0.2vw_#000000] transition-colors duration-300 ease-linear ${isOpen ? "bg-[#EA5571]" : "bg-[#CD4258]"}`
+          // "w-[1.7vw] h-[2vw] rounded-full bg-[#CD4258] drop-shadow-[0_0.3vw_0.2vw_#000000]" + (isOpen?" bg-[#EA5571]":" bg-[#CD4258]")
+        }
         />
         <div 
-        className={"min-h-[5.7vw] h-full translate-y-[0.5vw] w-[0.2vw] bg-[#CD4258] drop-shadow-[0_0.2vw_0.2vw_#000000]" + (RUNDOWN.length-1 == index?" hidden": "") + (isOpen?" bg-[#EA5571]":" bg-[#CD4258]")}
+        className={"min-h-[5.7vw] h-full translate-y-[1vw] w-[0.2vw] bg-[#CD4258] drop-shadow-[0_0.2vw_0.2vw_#000000]" + (RUNDOWN.length-1 == index?" hidden": "") + (isOpen?" bg-[#EA5571]":" bg-[#CD4258]")}
         />
         {/* <div 
         className={"bottom-0 translate-y-[vw] h-[1.2vw] w-[0.2vw] bg-[#CD4258] drop-shadow-[0_0.2vw_0.2vw_#000000]" + (RUNDOWN.length-1 == index?" hidden": "")}
         /> */}
       </div>
       <div
+      
       className={"w-[57.7vw] h-fit py-[0.9vw] items-center px-[2vw] font-publica-sans font-medium text-[1.7vw] rounded-[1.7vw] text-[#FFE5C7] ease-linear drop-shadow-[0_0.2vw_0.2vw_#000000] flex-shrink-0 duration-300 cursor-pointer" + (isOpen?" min-h-[12.7vw] bg-[#EA5571]":" bg-[#CD4258]")}
       >
         <p>{item.agenda}</p>
         <div
             className={
-              "text-[#FFE5C7] font-light text-[1.25vw] pt-5 transition-[grid-template-rows] duration-200 translate-y-[-15px] drop-shadow-lg grid select-none " +
-              (isOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr] !p-0 !pt-0")
+              `text-[#FFE5C7] font-light text-[1.25vw] transition-[max-height] duration-300 ease-linear overflow-hidden 
+              ${isOpen ? "max-h-[100px]" : "max-h-0 !p-0 !pt-0"}`
+              // "text-[#FFE5C7] font-light text-[1.25vw] pt-5 transition-[grid-template-rows] duration-200 translate-y-[-15px] drop-shadow-lg grid select-none " +
+              // (isOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr] !p-0 !pt-0")
             }
           >
             <p className="overflow-hidden w-full text-justify">{item.deskripsi}</p>
@@ -82,11 +95,16 @@ const Timeline = ({item, index} : {item:any, index:number}) => {
   )
 }
 
-//Kalo mau ngeubah jeda di sumbu y, ubah atribut gap-y-[2vw] 
+//Kalo mau ngeubah jeda di sumbu y, ubah atribut gap-y-[2vw] menjadi gap-y-[0.5vw]
 export const Day1 = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return(
     <div 
-    className="flex flex-col gap-y-[2vw] py-[3vw]">
+    data-aos="fade-up"
+    className="flex flex-col gap-y-[1vw] py-[3vw]">
       {RUNDOWN.map((item, index) =>(
         <Timeline 
         item={item} 
