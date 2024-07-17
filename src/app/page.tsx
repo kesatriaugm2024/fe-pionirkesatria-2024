97a8aaf82444f1078ac57b968a39a05dccea2314
Awan <itproject2k22@gmail.com>
2024-07-17 15:28:13 +0700

    feat: Chapter 2 in Beranda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import Chapter1 from "@/components/beranda/chapter1";
 import Chapter2 from "@/components/beranda/chapter2";
-// import Chapter3 from "@/components/beranda/chapter3";
+import Chapter3 from "@/components/beranda/chapter3";
 import SelamatDatang from "@/components/beranda/selamatdatang";
 import SelamatDatangVideo from "@/components/beranda/selamatdatangvideo";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[url('/images/background/Background_beranda.svg')] bg-repeat bg-[size:25%]">
-      <SelamatDatang/>
-      <SelamatDatangVideo/>
-      <Chapter1/>
+    <main className="min-h-screen">
+      {/* <SelamatDatang/> */}
+      {/* <SelamatDatangVideo/> */}
+      {/* <Chapter1/> */}
       <Chapter2/>
       {/* <Chapter3/> */}
     </main>
