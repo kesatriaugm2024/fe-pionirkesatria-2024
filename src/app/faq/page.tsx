385d65aa051d1bd7ffd50b2969dc61a67522b7d2
fahmiirfanfaiz <fahmiirfanfaiz01@gmail.com>
2024-07-19 08:16:57 +0700

    menambahkan faq

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,12 +1,46 @@
 "use client";
 import { FaPlus } from "react-icons/fa6";
-import { useState } from "react";
-import {
-  listPertanyaanUmum,
-  listPertanyaanMateri,
-} from "@/app/faq/listPertanyaan";
+import { useState, useEffect } from "react";
+import { listPertanyaanUmum, listPertanyaanMateri } from "@/app/faq/listPertanyaan";
 
 export default function DesignGuidelinePage() {
+  const [userInput, setUserInput] = useState("");
+  const [foundPertanyaanUmum, setFoundPertanyaanUmum] = useState(listPertanyaanUmum); 
+  const [foundPertanyaanMateri, setFoundPertanyaanMateri] = useState(listPertanyaanMateri); 
+
+  const checkSearchResult = () => {
+    let questionUmum = [];
+    let questionMateri = [];
+    listPertanyaanUmum.forEach((item) => {
+      let _question = item.question.toUpperCase();
+      if (_question.includes(userInput.toUpperCase())) {
+        questionUmum.push(item);
+      }
+    });
+    listPertanyaanMateri.forEach((item) => {
+      let _question = item.question.toUpperCase();
+      if (_question.includes(userInput.toUpperCase())) {
+        questionMateri.push(item);
+      }
+    });
+    setFoundPertanyaanUmum(questionUmum);
+    setFoundPertanyaanMateri(questionMateri);
+  };
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (userInput === "") {
+      setFoundPertanyaanUmum(listPertanyaanUmum);
+      setFoundPertanyaanMateri(listPertanyaanMateri);
+    } else {
+      checkSearchResult();
+    }
+  };
+
   return (
     <main className="min-h-screen w-full flex-col bg-faq-background bg-cover bg-top bg-repeat py-10">
       <div className="flex w-full flex-col items-center justify-center">
@@ -16,14 +50,14 @@ export default function DesignGuidelinePage() {
 
         <div className="bg-gradient-to-b from-[#5E1675] via-[#982D66] to-[#CD4258] bg-clip-text font-legendaire text-[2.5vw] text-transparent">
           Punya Pertanyaan?
-          <div className="flex h-[4vw] w-[86vw] items-center justify-between gap-[1vw] rounded-xl bg-[#5E1675] px-[1vw]">
+          <form className="flex h-[4vw] w-[86vw] items-center justify-between gap-[1vw] rounded-xl bg-[#5E1675] px-[1vw]" onSubmit={handleSubmit}>
             <div className="flex w-full items-center">
               <input
                 type="text"
                 placeholder="Cari di sini..."
-                // w-full bg-transparent border-none text-[1.5vw] text-[#FFFFFF] placeholder:text-[1.2vw] placeholder-[#FFB314] outline-none focus:outline-4 focus:outline-[#FFA514]
-                // w-full bg-transparent border-none text-[1.5vw] text-[#FFFFFF] placeholder:text-[1.2vw] placeholder-[#FFB314] outline-none focus:outline-4 focus:outline-[#FFA514]
                 className="w-full border-none bg-transparent font-publica-sans text-[1.5vw] text-[#FFB314] placeholder-[#FFB314] placeholder:text-[1.5vw] outline-none"
+                value={userInput} 
+                onChange={handleChange} 
               />
             </div>
             <button className="mr-3 h-[3vw] w-[7vw] items-end rounded-[1vw] bg-gradient-to-b from-[#FFD23F] to-[#FFA514]">
@@ -31,13 +65,13 @@ export default function DesignGuidelinePage() {
                 Search
               </div>
             </button>
-          </div>
+          </form>
           <div className="relative mt-[1.5vw] flex flex-col items-center rounded-xl bg-[#5E1675] py-[3vw]">
             <p className="bg-gradient-to-b from-[#FFD23F] to-[#FFA514] bg-clip-text font-legendaire text-[3vw] text-transparent drop-shadow-[0_4px_5px_rgba(0,0,0,0.7)]">
               Pertanyaan Umum
             </p>
             <div className="relative flex flex-col gap-[1.5vw]">
-              {listPertanyaanUmum.map((pertanyaan, index) => (
+              {foundPertanyaanUmum.map((pertanyaan, index) => ( 
                 <ItemPertanyaan
                   key={index}
                   question={`Q: ${pertanyaan.question}`}
@@ -76,7 +110,7 @@ export const ItemPertanyaan = ({
         <p className="px-[1vw] font-publica-sans text-[1.4vw] font-medium text-[#5E1675]">
           {question}
         </p>
-        <div className="h-4 w-4 items-end">
+        <div className="h-[1.25vw] w-[1.25vw] items-end">
           <FaPlus className="h-full text-black" />
         </div>
       </div>
