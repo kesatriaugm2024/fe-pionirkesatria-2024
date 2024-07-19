bc08ca4c33c1fd50656827742d0041ffacf03bee
Hifnie Nazih <hifninazih111@gmail.com>
2024-07-19 20:55:44 +0700

    Responsive agenda day 2

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -3,55 +3,60 @@ import { useState } from "react";
 import { Day1 } from "@/app/agenda/rundown/day1";
 import { Day2 } from "@/app/agenda/rundown/day2";
 import Image from "next/image";
+import Footer from "@/components/layout/footer";
 // import AOS from "aos";
 
 const BUTTONOPTION = [
   {
-    'name' : 'Day1',
-    'label' : 'Hari Pertama'
+    name: "Day1",
+    label: "Hari Pertama",
   },
   {
-    'name' : 'Day2',
-    'label' : 'Hari Kedua'
+    name: "Day2",
+    label: "Hari Kedua",
   },
-]
+];
 
 export default function Agenda() {
-  const [activePage, setActivePage] = useState("Day1")
+  const [activePage, setActivePage] = useState("Day1");
 
-  function handleDay1(){
-    setActivePage("Day1")
+  function handleDay1() {
+    setActivePage("Day1");
   }
 
-  function handleDay2(){
-    setActivePage("Day2")
+  function handleDay2() {
+    setActivePage("Day2");
   }
 
   return (
+    <>
+      <main className="-mb-[60vw] flex w-full flex-col bg-pattern-krem-kesat bg-[size:20%] bg-repeat pb-[50vw] pt-[10vw] md:-mb-[20vw] md:pb-[30vw]">
+        <h1 className="bg-gradient-to-t from-[#5E1675] via-[#982D66] to-[#CD4258] bg-clip-text text-center font-legendaire text-[15vw] text-transparent sm:text-[10vw] lg:text-[8vw]">
+          Agenda
+        </h1>
+        <div className="flex w-full justify-center gap-x-[1vw] pb-[2vw] pt-[2vw] md:pb-[1vw]">
+          {BUTTONOPTION.map((item, index) => (
+            <button
+              key={index}
+              className={
+                "md h-[8vw] w-[28vw] rounded-[10vw] font-publica-sans text-[3vw] font-medium text-[#FFE5C7] sm:h-[6vw] sm:w-[24vw] sm:text-[2.5vw] md:h-[5vw] md:text-[2vw] lg:h-[3.5vw] lg:w-[15vw] lg:text-[1.4vw] 2xl:h-[2.5vw] 2xl:w-[12vw] 2xl:text-[1vw] " +
+                (activePage == item.name
+                  ? "bg-[#EA5571]"
+                  : "bg-[#CD4258] hover:bg-[#EA5571]")
+              }
+              onClick={item.name == "Day1" ? handleDay1 : handleDay2}
+            >
+              {item.label}
+            </button>
+          ))}
+        </div>
 
-    <main 
-    className="bg-[url('/images/background/background-agenda.svg')] w-full bg-cover min-h-screen flex flex-col pt-[5vw]"
-    >
-      <h1 
-      className="font-legendaire bg-gradient-to-t from-[#5E1675] to-[#CD4258] via-[#982D66] text-[8.4vw] text-center bg-clip-text text-transparent"
-      >
-        Agenda
-      </h1>
-      <div className="w-full flex justify-center py-[1vw] gap-x-[4vw]">
-        {BUTTONOPTION.map((item, index) =>(
-          <button 
-          key={index}
-          className={"w-[20.8vw] h-[2.9vw] rounded-[1.7vw] font-publica-sans font-medium text-[#FFE5C7] text-[1.7vw] " + (activePage==item.name?'bg-[#EA5571] ' : 'bg-[#CD4258] hover:bg-[#EA5571]')}
-          onClick={item.name=='Day1'?handleDay1:handleDay2}
-          >
-            {item.label}
-          </button>
-        ))}
-      </div>
-      <div className="py-[1vw]">
-        {activePage=="Day1" && <Day1/>}
-        {activePage=="Day2" && <Day2/>}
-      </div>
-    </main>
-  ); 
+        <div className="py-[1vw]">
+          {activePage == "Day1" && <Day1 />}
+          {activePage == "Day2" && <Day2 />}
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
--- src/app/agenda/rundown/day2.tsx
+++ src/app/agenda/rundown/day2.tsx
@@ -1,65 +1,17 @@
 "use client";
 import { useState } from "react";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-interface RundownItem {
-  time: string;
-  agenda: string;
-  deskripsi: string;
-}
+import AOS from "aos";
+import "aos/dist/aos.css";
+import dataDay2 from "@/app/agenda/rundown/day2.json";
+import Image from "next/image";
 
 export const Day2 = () => {
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
-  const RUNDOWN: RundownItem[] = [
-    {
-      time: "07.00",
-      agenda: "Kedatangan Kesatria Muda",
-      deskripsi: "Kesatria Muda tiba di area Tugu Teknik dan melakukan pengecekan atribut serta persiapan awal Hari Kedua Pionir Kesatria 2024.",
-    },
-    {
-      time: "07.30",
-      agenda: "Pameran Karya",
-      deskripsi: "Pameran karya yang menampilkan hasil kreasi dan inovasi seluruh Tim di Fakultas Teknik Universitas Gadjah Mada serta berbagai proyek yang telah mereka kerjakan.",
-    },
-    {
-      time: "09.25",
-      agenda: "Talkshow",
-      deskripsi: "Sesi talkshow dengan pembicara inspiratif yang membahas topik-topik relevan bagi Kesatria Muda, diikuti sesi tanya jawab dari pembicara dan Kesatria Muda.",
-    },
-    {
-      time: "10.35",
-      agenda: "Kepemanduan",
-      deskripsi : "Kegiatan kepemanduan dalam ruangan yang dipimpin oleh pemandu, melanjutkan materi berikutnya dan ditutup dengan diskusi kelas.",
-    },
-    {
-      time: "11.25",
-      agenda: "ISHOMA",
-      deskripsi: "Kesatria Muda melakukan istirahat, sholat, dan makan secara bergantian.",
-    },
-    {
-      time: "12.40",
-      agenda: "Karya Aplikatif",
-      deskripsi: "Kesatria Muda mengerjakan proyek aplikasi nyata dari ilmu yang telah dipelajari, diikuti dengan presentasi hasil karya yang akan dinilai oleh Mahasiswa Berprestasi Fakultas Teknik Universitas Gadjah Mada.",
-    },
-    {
-      time: "13.40",
-      agenda: "Formasi Angkatan",
-      deskripsi: "Kesatria Muda membentuk formasi angkatan 3 warna di wilayah Tugu Teknik Universitas Gadjah Mada.",
-    },
-    {
-      time: "15.30",
-      agenda: "Upacara Penutupan Pionir Kesatria 2024",
-      deskripsi: "Upacara penutupan Pionir Kesatria 2024 dengan penyematan Korsa Teknik, sambutan penutup, diakhiri dengan pemutaran anthem dan penampilan Band Fakultas Teknik Universitas Gadjah Mada.",
-    },
-  ];
-
   return (
     <div
       data-aos="fade-up"
-      className="flex flex-col gap-3 w-[90%] sm:w-[70%] mx-auto lg:max-w-[700px] xl:max-w-[900px] items-center"
+      className="mx-auto flex w-[90%] flex-col items-center gap-3 sm:w-[70%] lg:max-w-[700px] xl:max-w-[900px]"
     >
-      {RUNDOWN.map((item, index) => {
+      {dataDay2.map((item, index) => {
         return (
           <AgendaItem
             key={index}
@@ -82,46 +34,75 @@ interface AgendaItemProps {
 const AgendaItem: React.FC<AgendaItemProps> = ({ time, agenda, desc }) => {
   const [state, setState] = useState<boolean>(false);
   return (
-    <div className="flex w-full flex-col sm:flex-row font-creato items-start xs:justify-start xs:items-start sm:gap-3 cursor-pointer">
-      <div className={`w-12 mt-1 font-publica-sans font-bold ${state ? 'text-[#EA5571]' : 'text-[#CD4258]'}`}>
+    <div
+      className="flex w-full cursor-pointer flex-col items-start justify-center font-publica-sans sm:flex-row sm:gap-3"
+      onClick={() => {
+        setState(!state);
+      }}
+    >
+      <div
+        className={`hidden w-12 font-publica-sans font-bold sm:mt-[1vw] sm:flex lg:mt-[0.8vw] 2xl:mt-[0.5vw] ${state ? "text-[#EA5571]" : "text-[#CD4258]"}`}
+      >
         {time}
       </div>
-      <div className={`w-[15px] h-[15px] mt-2 rounded-t-full rounded-b-full ${state ? 'bg-[#EA5571]' : 'bg-[#CD4258]'}`}>
-        
-      </div>
-      {/* <div className={`p-2 text-center flex-shrink-0 h-[40px] w-full sm:w-[150px] md:w-[173px] text-neutral-100 rounded-t-xl sm:rounded-xl drop-shadow-lg ${state ? 'bg-[#EA5571]' : 'bg-[#CD4258]'}`}> */}
-        {/* {time} */}
-      {/* </div> */}
+      {/* Buletan */}
+      {/* <div
+        className={`mt-2 h-[15px] w-[15px] rounded-full rounded-t-full ${state ? "bg-[#EA5571]" : "bg-[#CD4258]"}`}
+      ></div> */}
 
-
-      <div
-        className="w-full"
-        onClick={() => {
-          setState(!state);
-        }}
-      >
-        <div className={`w-full text-[#FFE5C7] rounded-b-xl sm:rounded-xl xs:flex xs:flex-col relative z-[1] ${state ? 'bg-[#EA5571]' : 'bg-[#CD4258]'}`}>
-          <div className="relative w-full p-2 flex xs:flex-row justify-between font-normal items-center select-none">
-            <p className="w-max font-bold">{agenda}</p>
-            <img
+      <div className="w-full">
+        <div
+          className={`xs:flex xs:flex-col relative z-[1] w-full rounded-xl text-[#FFE5C7] sm:rounded-xl ${state ? "bg-[#EA5571]" : "bg-[#CD4258]"}`}
+        >
+          <div className="xs:flex-row relative flex w-full select-none items-center justify-between p-2 font-normal">
+            <div className="flex flex-row gap-[2vw]">
+              <p className="w-[8vw] font-light sm:hidden">{time}</p>
+              <p className="w-max font-bold">{agenda}</p>
+            </div>
+            <div
               className={
-                "h-[10px] duration-200 flex-shrink-0 " + (state ? "rotate-0" : "rotate-180")
+                "h-[20px] w-[20px] flex-shrink-0 duration-200 " +
+                (state ? "rotate-0" : "rotate-180")
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 32 32"
+                id="up-arrow"
+                fill="#FFE5C7"
+              >
+                <path d="M26 22a2 2 0 0 1-1.41-.59L16 12.83l-8.59 8.58a2 2 0 0 1-2.82-2.82l10-10a2 2 0 0 1 2.82 0l10 10A2 2 0 0 1 26 22Z"></path>
+              </svg>
+            </div>
+            {/* <Image
+              src="/images/icon/up-arrow.svg"
+              alt=""
+              width={20}
+              height={20}
+              className={
+                "h-[20px] flex-shrink-0 duration-200 " +
+                (state ? "rotate-0" : "rotate-180")
+              }
+            /> */}
+            {/* <img
+              className={
+                "h-[10px] flex-shrink-0 duration-200 " +
+                (state ? "rotate-0" : "rotate-180")
               }
               src="/icon-up.png"
               alt=""
-            />
+            /> */}
           </div>
         </div>
         <div
           className={
-            "bg-[#EA5571] rounded-b-xl text-[#FFE5C7] p-2 pt-5 transition-[grid-template-rows] duration-200 translate-y-[-15px] drop-shadow-lg grid select-none " +
+            "grid translate-y-[-15px] select-none rounded-b-xl bg-[#EA5571] p-2 pt-5 text-[#FFE5C7] drop-shadow-lg transition-[grid-template-rows] duration-200 " +
             (state ? "grid-rows-[1fr]" : "grid-rows-[0fr] !p-0 !pt-0")
           }
         >
-          <p className="overflow-hidden w-full text-justify">{desc}</p>
+          <p className="w-full overflow-hidden text-justify">{desc}</p>
         </div>
       </div>
     </div>
   );
 };
-
