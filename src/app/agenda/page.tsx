ff81c10ce754dd87811e3e1e95f97d7c169bc099
Hifnie Nazih <hifninazih111@gmail.com>
2024-07-20 15:48:00 +0700

    Fix responsive agenda

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -34,16 +34,16 @@ export default function Agenda() {
 
   return (
     <>
-      <main className="-mb-[60vw] flex w-full flex-col bg-pattern-krem-kesat bg-[size:20%] bg-repeat pb-[50vw] pt-[10vw] md:-mb-[20vw] md:pb-[30vw]">
+      <main className="flex min-h-[100dvh] w-full flex-col bg-pattern-krem-kesat bg-[size:20%] bg-repeat py-[20vw] sm:py-[12vw] lg:py-[7vw]">
         <h1 className="bg-gradient-to-t from-[#5E1675] via-[#982D66] to-[#CD4258] bg-clip-text text-center font-legendaire text-[15vw] text-transparent sm:text-[10vw] lg:text-[8vw]">
           Agenda
         </h1>
-        <div className="flex w-full justify-center gap-x-[1vw] pb-[2vw] pt-[2vw] md:pb-[1vw]">
+        <div className="flex w-full justify-center gap-x-[1vw] pb-[2vw] pt-[2vw] md:pb-[1vw] md:pt-[1vw] lg:pb-[1vw]">
           {BUTTONOPTION.map((item, index) => (
             <button
               key={index}
               className={
-                "md h-[8vw] w-[28vw] rounded-[10vw] font-publica-sans text-[3vw] font-medium text-[#FFE5C7] sm:h-[6vw] sm:w-[24vw] sm:text-[2.5vw] md:h-[5vw] md:text-[2vw] lg:h-[3.5vw] lg:w-[15vw] lg:text-[1.4vw] 2xl:h-[2.5vw] 2xl:w-[12vw] 2xl:text-[1vw] " +
+                "h-[8vw] w-[28vw] rounded-[10vw] font-publica-sans text-[3vw] font-medium text-[#FFE5C7] sm:h-[6vw] sm:w-[24vw] sm:text-[2.5vw] md:h-[5vw] md:text-[2vw] lg:h-[3.5vw] lg:w-[15vw] lg:text-[1.4vw] 2xl:h-[2.5vw] 2xl:w-[12vw] 2xl:text-[1vw] " +
                 (activePage == item.name
                   ? "bg-[#EA5571]"
                   : "bg-[#CD4258] hover:bg-[#EA5571]")
@@ -55,7 +55,7 @@ export default function Agenda() {
           ))}
         </div>
 
-        <div className="py-[1vw]">
+        <div className="py-[1vw] sm:translate-x-[-3.5vw] sm:py-[2vw] lg:translate-x-[-2vw] lg:py-[1vw]">
           {activePage == "Day1" && <Day data={dataDay1} />}
           {activePage == "Day2" && <Day data={dataDay2} />}
         </div>
--- src/app/agenda/rundown/day.tsx
+++ src/app/agenda/rundown/day.tsx
@@ -32,26 +32,30 @@ const AgendaItem: React.FC<AgendaItemProps> = ({ time, agenda, desc }) => {
   const [state, setState] = useState<boolean>(false);
   return (
     <div
-      className="flex w-full cursor-pointer flex-col items-start justify-center font-publica-sans sm:flex-row sm:gap-3"
+      className="flex w-full cursor-pointer flex-col items-start justify-center font-publica-sans sm:flex-row sm:gap-[2vw] lg:gap-[0vw]"
       onClick={() => {
         setState(!state);
       }}
     >
       <div
-        className={`hidden w-12 font-publica-sans font-bold sm:mt-[1vw] sm:flex lg:mt-[0.8vw] 2xl:mt-[0.5vw] ${state ? "text-[#EA5571]" : "text-[#CD4258]"}`}
+        className={`hidden w-12 font-publica-sans font-bold sm:mt-[1.2vw] sm:flex sm:w-[8vw] sm:items-center sm:justify-center sm:text-[2.5vw] lg:mt-[0.5vw] lg:w-[6vw] lg:text-[1.3vw] ${state ? "text-[#EA5571]" : "text-[#CD4258]"}`}
       >
         {time}
       </div>
 
       <div className="w-full">
         <div
-          className={`xs:flex xs:flex-col relative z-[1] w-full rounded-xl text-[#FFE5C7] sm:rounded-xl ${state ? "bg-[#EA5571]" : "bg-[#CD4258]"}`}
+          className={`relative z-[1] flex w-full flex-col rounded-t-[3vw] text-[#FFE5C7] duration-200 sm:rounded-t-[2vw] lg:rounded-t-[1vw] ${state ? "rounded-b-none" : "rounded-b-[3vw] sm:rounded-b-[2vw] lg:rounded-b-[1vw]"} ${state ? "bg-[#EA5571]" : "bg-[#CD4258]"}`}
         >
-          <div className="xs:flex-row relative flex w-full select-none items-center justify-between p-2 font-normal">
-            <div className="flex flex-row gap-[2vw]">
-              <p className="w-[8vw] font-light sm:hidden">{time}</p>
-              <p className="w-max font-bold">{agenda}</p>
+          <div className="relative flex w-full select-none flex-row items-center justify-between p-[2.5vw] font-publica-sans font-normal sm:px-[2vw] sm:py-[1.5vw] lg:py-[0.8vw] lg:pl-[1.2vw] lg:pr-[1vw]">
+            <div className="flex flex-row items-center justify-start gap-[2vw]">
+              <p className="text-[3vw] font-bold sm:hidden">{time}</p>
+              <p className="w-max text-[3.2vw] font-normal sm:text-[2vw] lg:text-[1.2vw]">
+                {agenda}
+              </p>
             </div>
+
+            {/* Arrow */}
             <div
               className={
                 "h-[20px] w-[20px] flex-shrink-0 duration-200 " +
@@ -67,33 +71,17 @@ const AgendaItem: React.FC<AgendaItemProps> = ({ time, agenda, desc }) => {
                 <path d="M26 22a2 2 0 0 1-1.41-.59L16 12.83l-8.59 8.58a2 2 0 0 1-2.82-2.82l10-10a2 2 0 0 1 2.82 0l10 10A2 2 0 0 1 26 22Z"></path>
               </svg>
             </div>
-            {/* <Image
-              src="/images/icon/up-arrow.svg"
-              alt=""
-              width={20}
-              height={20}
-              className={
-                "h-[20px] flex-shrink-0 duration-200 " +
-                (state ? "rotate-0" : "rotate-180")
-              }
-            /> */}
-            {/* <img
-              className={
-                "h-[10px] flex-shrink-0 duration-200 " +
-                (state ? "rotate-0" : "rotate-180")
-              }
-              src="/icon-up.png"
-              alt=""
-            /> */}
           </div>
         </div>
         <div
           className={
-            "grid translate-y-[-15px] select-none rounded-b-xl bg-[#EA5571] p-2 pt-5 text-[#FFE5C7] drop-shadow-lg transition-[grid-template-rows] duration-200 " +
+            "grid -translate-y-[3vw] select-none rounded-b-[3vw] bg-[#EA5571] p-[2.5vw] text-[#FFE5C7] drop-shadow-lg transition-[grid-template-rows] duration-200 sm:-translate-y-[2.5vw] sm:rounded-b-[2vw] lg:-translate-y-[1.5vw] lg:rounded-b-[1vw] lg:px-[1.2vw] lg:py-[1.5vw] " +
             (state ? "grid-rows-[1fr]" : "grid-rows-[0fr] !p-0 !pt-0")
           }
         >
-          <p className="w-full overflow-hidden text-justify">{desc}</p>
+          <p className="w-full overflow-hidden text-justify text-[3vw] font-light sm:text-[2vw] lg:text-[1.2vw]">
+            {desc}
+          </p>
         </div>
       </div>
     </div>
