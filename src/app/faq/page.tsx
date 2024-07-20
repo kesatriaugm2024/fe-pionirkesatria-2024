978d047f54f7b87deebceb07ef1dee17476248d1
fahmiirfanfaiz <fahmiirfanfaiz01@gmail.com>
2024-07-20 17:35:55 +0700

    memperbaiki bug faq versi tablet

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -15,7 +15,7 @@ export default function DesignGuidelinePage() {
   }, []);
 
   return (
-    <main className="bg-pattern-krem-kesat bg-[size:25%] bg-repeat">
+    <main className="bg-pattern-krem-kesat bg-[size:25%] bg-repeat sm:pt-[16vw] lg:pb-[4vw] lg:pt-[0vw]">
       <>
         <FAQ_Desktop className="hidden lg:block" />
         <FAQ_Tablet className="hidden sm:block lg:hidden" />
@@ -41,12 +41,12 @@ const ItemPertanyaan = ({
   return (
     <div
       className={`flex w-[88vw] cursor-pointer flex-col items-start rounded-[1.5vw] bg-gradient-to-r from-[#B22635] to-[#EA5571] duration-200 sm:w-[90vw] lg:w-[80vw] ${
-        isOpen ? "h-[6vw]" : "h-[3vw]"
+        isOpen ? "h-[6vw] sm:h-[18vw]" : "h-[3vw] sm:h-[9vw]"
       }`}
       onClick={handleClick}
     >
-      <div className="flex h-[6vw] w-full items-center justify-between rounded-[1.5vw] bg-gradient-to-b from-[#FFA514] to-[#FFD23F] pr-[1.5vw] sm:h-[6vw] lg:h-[3vw]">
-        <p className="px-[1vw] font-publica-sans text-[1.4vw] font-medium text-[#5E1675] sm:text-[1.5vw] lg:text-[1.4vw]">
+      <div className="flex h-[16vw] w-full items-center justify-between rounded-[1.5vw] bg-gradient-to-b from-[#FFA514] to-[#FFD23F] pr-[1.5vw] sm:h-[9vw] lg:h-[3vw]">
+        <p className="px-[1vw] font-publica-sans text-[1.4vw] font-medium text-[#5E1675] sm:text-[3vw] lg:text-[1.4vw]">
           {question}
         </p>
         <div className="mr-[2vw] h-[2vw] w-[2vw] sm:mr-[2vw] sm:h-[1.5vw] sm:w-[1.5vw] sm:items-end lg:mr-[1vw] lg:h-[1.25vw] lg:w-[1.25vw] lg:items-end">
@@ -54,10 +54,10 @@ const ItemPertanyaan = ({
         </div>
       </div>
       <div
-        className={`h-[6vw] w-full items-center sm:h-[3vw] lg:h-[3vw] ${isOpen ? "flex" : "hidden"}`}
+        className={`h-[6vw] w-full items-center sm:h-[3vw] sm:items-start lg:h-[3vw] ${isOpen ? "flex" : "hidden"}`}
       >
         <p
-          className={`px-[1vw] font-publica-sans text-[1.4vw] font-medium gradient-text-yellow-up-down sm:text-[1.5vw] lg:text-[1.4vw] ${isOpen ? "block" : "hidden"}`}
+          className={`px-[1vw] font-publica-sans text-[1.4vw] font-medium gradient-text-yellow-up-down sm:text-[3vw] lg:text-[1.4vw] ${isOpen ? "block" : "hidden"}`}
         >
           {answer}
         </p>
@@ -217,6 +217,7 @@ function FAQ_Tablet({ className }: { className?: string }) {
   }, []);
   return (
     // min-h-screen relative w-full flex-col bg-cover bg-top
+    // relative w-full h-auto flex-col bg-cover bg-top ${className}
     <main
       className={`relative min-h-screen w-full flex-col bg-cover bg-top ${className} `}
     >
@@ -230,46 +231,46 @@ function FAQ_Tablet({ className }: { className?: string }) {
       <div className={`flex w-full flex-col items-center justify-center`}>
         <h1
           data-aos="fade-up"
-          className="z-[1] ml-[4vw] mt-[24vw] self-center bg-gradient-to-b from-[#5E1675] via-[#982D66] to-[#CD4258] bg-clip-text font-legendaire text-[7vw] text-transparent"
+          className="z-[1] ml-[4vw] mt-[26vw] self-center bg-gradient-to-b from-[#5E1675] via-[#982D66] to-[#CD4258] bg-clip-text font-legendaire text-[10vw] text-transparent"
         >
           Frequently
         </h1>
         <h1
           data-aos="fade-up"
-          className="z-[1] ml-[4vw] mt-[-3vw] self-center bg-gradient-to-b from-[#5E1675] via-[#982D66] to-[#CD4258] bg-clip-text font-legendaire text-[7vw] text-transparent"
+          className="z-[1] ml-[4vw] mt-[-3vw] self-center bg-gradient-to-b from-[#5E1675] via-[#982D66] to-[#CD4258] bg-clip-text font-legendaire text-[10vw] text-transparent"
         >
           Asked Question
         </h1>
 
         <div
           data-aos="fade-up"
-          className="z-[1] bg-gradient-to-b from-[#5E1675] via-[#982D66] to-[#CD4258] bg-clip-text font-legendaire text-[4vw] text-transparent"
+          className="z-[1] bg-gradient-to-b from-[#5E1675] via-[#982D66] to-[#CD4258] bg-clip-text font-legendaire text-[6vw] text-transparent"
         >
           Punya Pertanyaan?
           <form
-            className="flex h-[5vw] w-[94vw] items-center justify-between gap-[1vw] rounded-xl bg-[#5E1675] px-[1vw]"
+            className="flex h-[6vw] w-[94vw] items-center justify-between gap-[1vw] rounded-xl bg-[#5E1675] px-[1vw]"
             onSubmit={handleSubmit}
           >
             <div className="flex w-full items-center">
               <input
                 type="text"
                 placeholder="Cari di sini..."
-                className="w-full border-none bg-transparent font-publica-sans text-[2vw] text-[#FFB314] placeholder-[#FFB314] outline-none placeholder:text-[2vw]"
+                className="ml-[2vw] w-full border-none bg-transparent font-publica-sans text-[3vw] text-[#FFB314] placeholder-[#FFB314] outline-none placeholder:text-[3vw]"
                 value={userInput}
                 onChange={handleChange}
               />
             </div>
-            <button className="z-[1] mr-[1vw] h-[3vw] w-[7vw] items-end rounded-[1vw] bg-gradient-to-b from-[#FFD23F] to-[#FFA514]">
-              <div className="bg-gradient-to-b from-[#491772] to-[#5E1675] bg-clip-text font-legendaire text-[2vw] text-transparent">
+            <button className="z-[1] mr-[1vw] h-[3.5vw] w-[9vw] items-end rounded-[1vw] bg-gradient-to-b from-[#FFD23F] to-[#FFA514]">
+              <div className="bg-gradient-to-b from-[#491772] to-[#5E1675] bg-clip-text font-legendaire text-[2.5vw] text-transparent">
                 Search
               </div>
             </button>
           </form>
           <div className="relative mt-[1.5vw] flex flex-col items-center rounded-xl bg-[#5E1675] py-[3vw] hover:bg-[#5E1675]">
-            <p className="mt-[-2vw] bg-gradient-to-b from-[#FFD23F] to-[#FFA514] bg-clip-text font-legendaire text-[3vw] text-transparent drop-shadow-[0_4px_5px_rgba(0,0,0,0.7)]">
+            <p className="mt-[-2vw] bg-gradient-to-b from-[#FFD23F] to-[#FFA514] bg-clip-text font-legendaire text-[5vw] text-transparent drop-shadow-[0_4px_5px_rgba(0,0,0,0.7)]">
               Pertanyaan Umum
             </p>
-            <div className="relative flex flex-col gap-[1.5vw]">
+            <div className="relative flex flex-col gap-[4vw]">
               {foundPertanyaanUmum.map((pertanyaan, index) => (
                 <ItemPertanyaan
                   key={index}
