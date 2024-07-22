63505c56bdac0d03756dfed19e778f09fbc0bb09
Hifnie Nazih <hifninazih111@gmail.com>
2024-07-22 11:31:09 +0700

    Fix modal dokumentasi

--- src/components/dokumentasi/image-modal.tsx
+++ src/components/dokumentasi/image-modal.tsx
@@ -37,19 +37,23 @@ export default function ImageModal({
 
   return (
     <div
-      className="min-w-screen fixed inset-0 left-0 top-0 z-[1000] flex h-screen items-center justify-center bg-cover bg-center bg-no-repeat outline-none transition duration-150 ease-in-out focus:outline-none"
+      className="min-w-screen fixed inset-0 left-0 top-0 z-[1000] flex h-screen items-center justify-center outline-none transition-all duration-150 ease-in-out"
       id="modal-id"
     >
       <div className="z-1 absolute inset-0 bg-black opacity-80"></div>
-      <div className="relative mx-5 my-auto w-full max-w-md rounded-xl bg-gradient-to-b from-[#FFD23F] to-[#FFA514] p-2 shadow-lg sm:mx-10 sm:max-w-3xl md:mx-20 md:p-3 lg:max-w-6xl lg:p-4">
+      <div className="relative w-full max-w-[90vw] rounded-[4.5vw] bg-gradient-to-b from-[#FFD23F] to-[#FFA514] p-[1.5vw] sm:max-w-[80vw] sm:rounded-[2vw] sm:p-[1vw] lg:max-w-[55vw] lg:rounded-[1vw] lg:p-[1vw]">
         {/* Image Content */}
         <div className="">
-          <Image src={image} alt="image" className="h-full w-full rounded-xl" />
+          <Image
+            src={image}
+            alt="image"
+            className="h-full w-full rounded-[3.5vw] sm:rounded-[2vw] lg:rounded-[1vw]"
+          />
         </div>
 
         {/* Close Button */}
-        <div
-          className="absolute right-3 top-3 -mr-6 -mt-6 h-10 w-10 rounded-full bg-gradient-to-b from-[#EA5571] to-[#843040] transition hover:scale-110 hover:cursor-pointer active:scale-100"
+        {/* <div
+          className="absolute right-[2vw] top-[2vw] -mr-6 -mt-6 h-10 w-10 rounded-full bg-gradient-to-b from-[#EA5571] to-[#843040] transition hover:scale-110 hover:cursor-pointer active:scale-100"
           onClick={() => setModalIsOpen(false)}
         >
           <svg
@@ -64,7 +68,7 @@ export default function ImageModal({
           >
             <path d="M 7.21875 5.78125 L 5.78125 7.21875 L 14.5625 16 L 5.78125 24.78125 L 7.21875 26.21875 L 16 17.4375 L 24.78125 26.21875 L 26.21875 24.78125 L 17.4375 16 L 26.21875 7.21875 L 24.78125 5.78125 L 16 14.5625 Z"></path>
           </svg>
-        </div>
+        </div> */}
       </div>
     </div>
   );
